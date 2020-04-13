0bda25826b689f9467a34cbfffa4c464f7a96b02
Maria F <myannyax@gmail.com>
2020-04-13 21:21:03 +0300

    refactor: ExceptionDescriptor

--- core/src/main/kotlin/com/samples/verifier/model/ExceptionDescriptor.kt
+++ core/src/main/kotlin/com/samples/verifier/model/ExceptionDescriptor.kt
@@ -1,39 +1,17 @@
 package com.samples.verifier.model
 
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.databind.JsonSerializer
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializerProvider
-import com.fasterxml.jackson.databind.module.SimpleModule
-import java.io.IOException
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 data class ExceptionDescriptor(
-    val message: String? = null,
-    val fullName: String? = null,
-    val stackTrace: List<StackTraceElement> = emptyList(),
-    val cause: ExceptionDescriptor? = null,
-    val localizedMessage: String? = null
+    val message: String?,
+    val fullName: String?,
+    val stackTrace: List<StackTraceElement>,
+    val cause: ExceptionDescriptor?,
+    val localizedMessage: String?
 )
 
-class ThrowableSerializer : JsonSerializer<Throwable>() {
-    @Throws(IOException::class)
-    override fun serialize(value: Throwable, gen: JsonGenerator, serializers: SerializerProvider) {
-        gen.writeStartObject()
-        gen.writeStringField("message", value.message)
-        gen.writeStringField("fullName", value.javaClass.name)
-        gen.writeObjectField("stackTrace", value.stackTrace?.take(3))
-        gen.writeObjectField("cause", if (value.cause != value) value.cause else null)
-        gen.writeEndObject()
-    }
-}
-
-val outputMapper = ObjectMapper().apply {
-    registerModule(SimpleModule().apply {
-        addSerializer(Throwable::class.java, ThrowableSerializer())
-    })
-}
-
 fun Throwable.toExceptionDescriptor(): ExceptionDescriptor {
-    val rawException = outputMapper.writeValueAsString(this)
-    return outputMapper.readValue(rawException, ExceptionDescriptor::class.java)
+    val mapper = jacksonObjectMapper()
+    val rawException = mapper.writeValueAsString(this)
+    return mapper.readValue(rawException, ExceptionDescriptor::class.java)
 }
