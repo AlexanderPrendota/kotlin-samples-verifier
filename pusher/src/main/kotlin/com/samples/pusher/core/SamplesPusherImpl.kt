3b1eeff39c3e835ea95b2171079c13e6e59d1f86
vmishenev <vad-mishenev@yandex.ru>
2021-04-12 21:21:01 +0300

    added config; refactor

--- pusher/src/main/kotlin/com/samples/pusher/core/SamplesPusherImpl.kt
+++ pusher/src/main/kotlin/com/samples/pusher/core/SamplesPusherImpl.kt
@@ -3,6 +3,7 @@ package com.samples.pusher.core
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.samples.pusher.core.model.PusherConfiruration
 import com.samples.pusher.core.utils.cloneRepository
 import com.samples.verifier.Code
 import com.samples.verifier.GitException
@@ -11,68 +12,47 @@ import org.eclipse.egit.github.core.PullRequest
 import org.eclipse.egit.github.core.PullRequestMarker
 import org.eclipse.egit.github.core.RepositoryId
 import org.eclipse.egit.github.core.client.GitHubClient
+import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.transport.CredentialsProvider
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.lang.Exception
 import kotlin.random.Random.Default.nextInt
 
+//data class AuthData(val name: String, val password: String)
 
+typealias CollectionSamples = Map<Code, ExecutionResult>
 internal class SamplesPusher(val url: String, val path: String,
-                             val username: String, val password: String = "",
+                             val user: String, val password: String = "",
                              val branch: String = "master") {
     private val logger = LoggerFactory.getLogger("Samples Pusher")
 
+    var configuraton: PusherConfiruration = PusherConfiruration()
+    fun readConfigFromFile(filename:String): SamplesPusher {
+        configuraton.readFromFile(filename)
+        return this
+    }
     fun push(inputFile: String) {
         val mapper = jacksonObjectMapper()
-        val res = mapper.readValue(File(inputFile), object : TypeReference<Map<Code, ExecutionResult>>() {})
+        val res = mapper.readValue(File(inputFile), object : TypeReference<CollectionSamples>() {})
         val dir = File(url.substringAfterLast('/').substringBeforeLast('.'))
 
         try {
-
             val git = cloneRepository(dir, url, branch)
-            val dirSamples = File(dir.path + File.pathSeparator + path)
-            if (!dirSamples.exists())
-                if (!dirSamples.mkdirs()) {
-                    logger.error("Can't directory by path ${dirSamples.path}")
-                    return
-                }
+            val dirSamples = prepareTargetPath(dir)
 
-
-            logger.debug("Created the path ${dirSamples.path}")
             val branchName = "new-branch-${nextInt()}"
             git.checkout().setCreateBranch(true).setName(branchName).call()
 
-            var i = 0
-            res.forEach { File(dirSamples, "${++i}.kt").writeText(it.key) }
+            writeSnippets(dirSamples, res)
+            logger.debug(".kt files are  written")
 
-            logger.debug(".kt files are  writed ")
-
-
-            git.add().addFilepattern(".").call()
-            git.commit().setAll(true).setAllowEmpty(true).setMessage("New samples").setCommitter("pusher", "bot@samples.kotlin.com").call()
-
-            val credentialsProvider: CredentialsProvider = UsernamePasswordCredentialsProvider(username, password)
-            git.push().setRemote(url).setCredentialsProvider(credentialsProvider).call()
-
+            commitAndPush(git)
             logger.debug(".kt are pushed")
 
-            val client = GitHubClient()
-
-            if(password.isEmpty())
-                client.setOAuth2Token(username)
-            else
-                client.setCredentials(username, password)
-
-            val prServise = org.eclipse.egit.github.core.service.PullRequestService(client)
-            var pr = PullRequest()
-            pr.setTitle("New samples ")
-            pr.setBody("New files")
-            pr.setBase(PullRequestMarker().setLabel(branchName))
-            pr.setHead(PullRequestMarker().setLabel("master"))
-            logger.info(RepositoryId.createFromUrl(url).generateId())
-            pr = prServise.createPullRequest(RepositoryId.createFromUrl(url), pr)
-            logger.debug("push request  is created ${pr.url}")
+            val client = createGHClient()
+            createPR(client, branchName)
 
         } catch (e: GitException) {
             logger.error("${e.message}")
@@ -83,6 +63,51 @@ internal class SamplesPusher(val url: String, val path: String,
                 dir.delete()
             }
         }
+    }
+
+    private fun prepareTargetPath(repoDir: File): File {
+        val dirSamples = File(repoDir.path ).resolve(path)
+        if (!dirSamples.exists())
+            if (!dirSamples.mkdirs()) {
+                throw Exception("Can't directory by path ${dirSamples.path}")
+            }
+
+        logger.debug("Created the path ${dirSamples.path}")
+        return dirSamples
+    }
+    private fun writeSnippets(dirSamples:File, res:CollectionSamples) {
+        var i = 0
+        res.forEach { File(dirSamples, "${++i}.kt").writeText(it.key) }
+    }
+
+    private fun commitAndPush(git: Git) {
+        git.add().addFilepattern(".").call()
+        git.commit().setAll(true).setAllowEmpty(true).setMessage("New samples").setCommitter(configuraton.committerName, configuraton.committerEmail).call()
+
+        val credentialsProvider: CredentialsProvider = UsernamePasswordCredentialsProvider(user, password)
+        git.push().setRemote(url).setCredentialsProvider(credentialsProvider).call()
+    }
+
+    private fun createGHClient(): GitHubClient {
+        val client = GitHubClient()
+
+        if(password.isEmpty())
+            client.setOAuth2Token(user)
+        else
+            client.setCredentials(user, password)
+        return client
+    }
+
+    private fun createPR(client:GitHubClient, baseBranch: String) {
+        val prServise = org.eclipse.egit.github.core.service.PullRequestService(client)
+        var pr = PullRequest()
+        pr.setTitle("New samples")
+        pr.setBody("New files")
+        pr.setBase(PullRequestMarker().setLabel(baseBranch))
+        pr.setHead(PullRequestMarker().setLabel(configuraton.headBranchPR))
+        logger.info(RepositoryId.createFromUrl(url).generateId())
+        pr = prServise.createPullRequest(RepositoryId.createFromUrl(url), pr)
+        logger.debug("Push request  is created ${pr.url}")
     }
 
 }
---
+++ pusher/src/main/kotlin/com/samples/pusher/core/model/PusherConfiruration.kt
@@ -0,0 +1,37 @@
+package com.samples.pusher.core.model
+
+import kotlin.jvm.javaClass
+import org.apache.commons.configuration2.FileBasedConfiguration
+import org.apache.commons.configuration2.PropertiesConfiguration
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder
+import org.apache.commons.configuration2.builder.fluent.Parameters
+import org.apache.commons.configuration2.ex.ConfigurationException
+
+class PusherConfiruration {
+
+    /**
+     *committer's  name and email
+     */
+    var committerName = "bot"
+    var committerEmail= "bot@samples.kotlin.com"
+
+    /**
+     *     what branch will the pull request be created for
+     */
+    var headBranchPR= "master"
+
+
+    fun readFromFile(filename: String) {
+        val params =  Parameters()
+        val builder =
+            FileBasedConfigurationBuilder<FileBasedConfiguration>( PropertiesConfiguration::class.java )
+                .configure(params.properties().setFileName(filename))
+
+        val config = builder.getConfiguration()
+        committerName =  config.getString("committerName",  committerName)
+        committerEmail = config.getString("committerEmail", committerEmail)
+        headBranchPR =   config.getString("headBranchPR",   headBranchPR)
+
+    }
+
+}
