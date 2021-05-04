969c15578ef9eeb8c31b6cd1aa8dd365337679cf
vmishenev <vad-mishenev@yandex.ru>
2021-05-04 16:22:22 +0300

    added working through the verifier library

--- pusher/src/main/kotlin/com/samples/pusher/core/SnippetManager.kt
+++ pusher/src/main/kotlin/com/samples/pusher/core/SnippetManager.kt
@@ -1,65 +1,67 @@
 package com.samples.pusher.core
+
 import org.slf4j.LoggerFactory
+import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
-import java.io.File
-
-fun md5(input:String): String {
-    val md = MessageDigest.getInstance("MD5")
-    return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+fun md5(input: String): String {
+  val md = MessageDigest.getInstance("MD5")
+  return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
 }
 
 class SnippetManager(val dirSamples: File) {
-    private val logger = LoggerFactory.getLogger("Samples Pusher")
+  private val logger = LoggerFactory.getLogger("Samples Pusher")
 
-    data class PathEntity(var countSnippets: Int, var hash: String)
+  data class PathEntity(var countSnippets: Int, var hash: String)
 
-    private val mapPath = mutableMapOf<String, PathEntity>()
+  private val mapPath = mutableMapOf<String, PathEntity>()
 
-    var changed: Boolean = false
-        private set
+  var changed: Boolean = false
+    private set
 
-    /*
-    Create a snippet file in the folder dirSamples/FileName
-    Example: for snippet from "src/test.html"
-    It will create some .kt file in dirSamples/test
-     */
-    fun addSnippet(code: String, path: String) {
-        val filename = path.substringAfterLast('/').substringBeforeLast('.')
+  /*
+  Create a snippet file in the folder dirSamples/FileName
+  Example: for snippet from "src/test.html"
+  It will create some .kt file in dirSamples/test
+   */
+  fun addSnippet(code: String, path: String) {
+    val filename = path.substringAfterLast('/').substringBeforeLast('.')
 
-        val entity = mapPath.getOrPut(path) { removeAllSnippets(path)
-                                              PathEntity(0, md5(path)) }
-        entity.countSnippets++
-        val newName = "${entity.hash}.${entity.countSnippets}.kt"
-        val targetDir = dirSamples.resolve(filename)
-        if(!targetDir.exists())
-            if(!targetDir.mkdirs())
-                throw Exception("Can't make dirs to ${targetDir.path}")
-
-        File(targetDir, newName).writeText(code)
-        changed = true
-        logger.info("Created a snippet file: ${newName}")
+    val entity = mapPath.getOrPut(path) {
+      removeAllSnippets(path)
+      PathEntity(0, md5(path))
     }
+    entity.countSnippets++
+    val newName = "${entity.hash}.${entity.countSnippets}.kt"
+    val targetDir = dirSamples.resolve(filename)
+    if (!targetDir.exists())
+      if (!targetDir.mkdirs())
+        throw Exception("Can't make dirs to ${targetDir.path}")
 
-    fun removeAllSnippets(path: String) {
-        val filename = path.substringAfterLast('/').substringBeforeLast('.')
-        val hash = md5(path)
-        val directory = dirSamples.resolve(filename)
-        var folder_empty = true
-        val fileTree = directory.walkTopDown()
-        for (file in fileTree) {
-            if (file.name.startsWith(hash)) {
-                if (!file.delete()) {
-                    logger.error("Can't remove the snippet file: ${file.name}")
-                }
-                changed = true
-                logger.info("Removed the snippet file: ${file.name}")
-            } else {
-                folder_empty = false
-            }
+    File(targetDir, newName).writeText(code)
+    changed = true
+    logger.info("Created a snippet file: ${newName}")
+  }
+
+  fun removeAllSnippets(path: String) {
+    val filename = path.substringAfterLast('/').substringBeforeLast('.')
+    val hash = md5(path)
+    val directory = dirSamples.resolve(filename)
+    var folder_empty = true
+    val fileTree = directory.walkTopDown()
+    for (file in fileTree) {
+      if (file.name.startsWith(hash)) {
+        if (!file.delete()) {
+          logger.error("Can't remove the snippet file: ${file.name}")
         }
-        if(folder_empty)
-            directory.delete()
+        changed = true
+        logger.info("Removed the snippet file: ${file.name}")
+      } else {
+        folder_empty = false
+      }
     }
+    if (folder_empty)
+      directory.delete()
+  }
 }
--- pusher/src/main/kotlin/com/samples/pusher/core/model/PusherConfiruration.kt
+++ pusher/src/main/kotlin/com/samples/pusher/core/model/PusherConfiruration.kt
@@ -1,6 +1,5 @@
 package com.samples.pusher.core.model
 
-import kotlin.jvm.javaClass
 import org.apache.commons.configuration2.FileBasedConfiguration
 import org.apache.commons.configuration2.PropertiesConfiguration
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder
@@ -9,29 +8,36 @@ import org.apache.commons.configuration2.ex.ConfigurationException
 
 class PusherConfiruration {
 
-    /**
-     *committer's  name and email
-     */
-    var committerName = "bot"
-    var committerEmail= "bot@samples.kotlin.com"
-
-    /**
-     *     what branch will the pull request be created for
-     */
-    var baseBranchPR= "master"
+  /**
+   *committer's  name and email
+   */
+  var committerName = "bot"
+  var committerEmail = "bot@samples.kotlin.com"
 
 
-    fun readFromFile(filename: String) {
-        val params =  Parameters()
-        val builder =
-            FileBasedConfigurationBuilder<FileBasedConfiguration>( PropertiesConfiguration::class.java )
-                .configure(params.properties().setFileName(filename))
+  /**
+   *commit message
+   */
+  var commitMsg = "New samples"
 
-        val config = builder.getConfiguration()
-        committerName =  config.getString("committerName",  committerName)
-        committerEmail = config.getString("committerEmail", committerEmail)
-        baseBranchPR =   config.getString("baseBranchPR",   baseBranchPR)
+  /**
+   *     what branch will the pull request be created for
+   */
+  var baseBranchPR = "master"
 
-    }
+
+  fun readFromFile(filename: String) {
+    val params = Parameters()
+    val builder =
+      FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration::class.java)
+        .configure(params.properties().setFileName(filename))
+
+    val config = builder.getConfiguration()
+    committerName = config.getString("committerName", committerName)
+    committerEmail = config.getString("committerEmail", committerEmail)
+    commitMsg = config.getString("commitMsg", commitMsg)
+    baseBranchPR = config.getString("baseBranchPR", baseBranchPR)
+
+  }
 
 }
