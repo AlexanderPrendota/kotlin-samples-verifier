d80dffaaf29fc277f1abf027df37e50f4342f36a
vmishenev <vad-mishenev@yandex.ru>
2021-05-06 15:05:14 +0300

    added new parameters: template-path config-path

--- pusher/src/main/kotlin/com/samples/pusher/core/model/PusherConfiruration.kt
+++ pusher/src/main/kotlin/com/samples/pusher/core/model/PusherConfiruration.kt
@@ -4,40 +4,47 @@ import org.apache.commons.configuration2.FileBasedConfiguration
 import org.apache.commons.configuration2.PropertiesConfiguration
 import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder
 import org.apache.commons.configuration2.builder.fluent.Parameters
-import org.apache.commons.configuration2.ex.ConfigurationException
+import java.net.URL
 
 class PusherConfiruration {
 
-  /**
-   *committer's  name and email
-   */
-  var committerName = "bot"
-  var committerEmail = "bot@samples.kotlin.com"
+    /**
+     *  committer's  name and email
+     */
+    var committerName = "bot"
+    var committerEmail = "bot@samples.kotlin.com"
 
 
-  /**
-   *commit message
-   */
-  var commitMsg = "New samples"
+    /**
+     *  commit message
+     */
+    var commitMsg = "New samples"
 
-  /**
-   *     what branch will the pull request be created for
-   */
-  var baseBranchPR = "master"
+    /**
+     *     what branch will the pull request be created for
+     */
+    var baseBranchPR = "master"
 
 
-  fun readFromFile(filename: String) {
-    val params = Parameters()
-    val builder =
-      FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration::class.java)
-        .configure(params.properties().setFileName(filename))
+    fun readFromFile(filename: String) {
+        val props = Parameters().properties()
+        if (filename.indexOf("http://", 0, true) == 0 ||
+            filename.indexOf("https://", 0, true) == 0
+        ) {
+            props.setURL(URL(filename))
+        } else {
+            props.setFileName(filename)
+        }
+        val builder =
+            FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration::class.java)
+                .configure(props)
 
-    val config = builder.getConfiguration()
-    committerName = config.getString("committerName", committerName)
-    committerEmail = config.getString("committerEmail", committerEmail)
-    commitMsg = config.getString("commitMsg", commitMsg)
-    baseBranchPR = config.getString("baseBranchPR", baseBranchPR)
+        val config = builder.getConfiguration()
+        committerName = config.getString("committerName", committerName)
+        committerEmail = config.getString("committerEmail", committerEmail)
+        commitMsg = config.getString("commitMsg", commitMsg)
+        baseBranchPR = config.getString("baseBranchPR", baseBranchPR)
 
-  }
+    }
 
 }
