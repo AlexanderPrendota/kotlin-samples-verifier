f614e43bec5af7b053b568d053078f68e20cfabd
vmishenev <vad-mishenev@yandex.ru>
2021-05-20 16:32:41 +0300

    Change PR text

---
+++ pusher/src/test/kotlin/com/samples/pusher/TemplatesTest.kt
@@ -0,0 +1,34 @@
+package com.samples.pusher
+
+import com.samples.pusher.core.Snippet
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import com.samples.pusher.core.TemplateManager
+import com.samples.verifier.Code
+import com.samples.verifier.model.CollectionOfRepository
+import com.samples.verifier.model.DiffOfRepository
+import com.samples.verifier.model.ErrorDescriptor
+import com.samples.verifier.model.ExecutionResult
+import java.util.*
+
+class TemplatesTest {
+    @Test
+    fun `templates test`() {
+        val templates = TemplateManager()
+        //templates.configureTemplate("https://raw.githubusercontent.com/vmishenev/kotlin-web-site/master/.github/workflows/")
+        templates.configureTemplate("templates")
+
+        val res = CollectionOfRepository("url", "",
+            HashMap<Code, ExecutionResult>(), DiffOfRepository("", "dsf", listOf<String>()))
+
+        val model = HashMap<String, Any>()
+        model.put("src", res)
+        model.put("badSnippets", listOf<Snippet>( Snippet("dfd", ExecutionResult( listOf<ErrorDescriptor>(), null, "kllkgdfg.md"  ) )))
+        model.put("snippets", listOf<Snippet>( Snippet("dfd", ExecutionResult( listOf<ErrorDescriptor>(), null, "kllkgdfg.md"  ) ) ))
+
+        val temp = templates.getTemplate("pr.md", model)
+        //print(temp)
+        Assertions.assertEquals(temp.head, "New samples from commit")
+    }
+}
--- pusher/templates/pr.md
+++ pusher/templates/pr.md
@@ -1,24 +1,26 @@
-New samples 
+New samples<#if src.diff?has_content> from commit</#if>
 
 New samples in a repository ${src.url}
+<#if src.diff?has_content>Commit: ${src.url}/commit/${src.diff.endRef}</#if>
 Files:
 <#list src.snippets as code, value>
-File: ${value.fileName}
+ ${value.fileName}
     <#--  ```${code}```  -->
 </#list>
-
+<#if badSnippets?has_content>
 Bad samples:
-<#list badSnippets as item>
-  File: ${item.res.fileName}
-
-```kotlin
-${item.code}
-```
-
-<#list item.res.errors as err>
-(${err.interval.start.line}:${err.interval.start.ch}, ${err.interval.end.line}:${err.interval.end.ch}) **${err.severity}** ${err.message}
-</#list>
+    <#list badSnippets as item>
+      File: ${item.res.fileName}
     
-------
-
-</#list>
+    ```kotlin
+    ${item.code}
+    ```
+    
+    <#list item.res.errors as err>
+    (${err.interval.start.line}:${err.interval.start.ch}, ${err.interval.end.line}:${err.interval.end.ch}) **${err.severity}** ${err.message}
+    </#list>
+        
+    ------
+    
+    </#list>
+</#if>
